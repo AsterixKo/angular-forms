9976f5150b8f7bbc356795a4846f8ec436d6a158
MSI\moneo <jvr.moneo@gmail.com>
2020-10-27 12:25:14 +0100

    hacer formulario mediante template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './components/template-form/template-form.component';
 import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ReactiveFormComponent } from './components/reactive-form/reactive-form.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/template-form/template-form.component.ts
+++ src/app/components/template-form/template-form.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  saveForm(formdata) {
+    console.log('saveForm...');
+
+    // if (formdata.status === 'INVALID') {
+    //   console.log('El formulario es inválido');
+    //   return;
+    // }
+
+    console.log('Este es el form data:', formdata);
+
+    // console.log('Los valores del formulario son', formdata.value);
+
+    // console.log('El nombre del usuario es', formdata.value.nombre);
+
+    // this.nombre = formdata.value.nombre;
+
+    const { nombre, apellido, email}=formdata.value;
+    this.firstName = nombre;
+    this.lastName = apellido;
+    this.emailAddress = email;
   }
 
 }
